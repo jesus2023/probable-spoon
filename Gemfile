2dbcb0f42005d50fb01c89918510dc8c02925324
Jesús2023 <dmirandaj@uninorte.edu.com.co>
2023-12-26 13:48:33 -0800

    primera aplicación

--- Gemfile
+++ Gemfile
@@ -58,7 +58,8 @@ group :development do
 
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
-
+  gem 'hamlit-rails'
+  gem 'simple_form'
   gem "error_highlight", ">= 0.4.0", platforms: [:ruby]
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_26_142505) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_26_202740) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
@@ -24,6 +24,16 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_26_142505) do
   create_table "pets", force: :cascade do |t|
     t.string "name"
     t.string "breed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.text "about_me"
+    t.integer "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
